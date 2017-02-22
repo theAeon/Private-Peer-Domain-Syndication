70c1bddf19567e7310c20332a56bdd43b4ef706e
Andrew Donshik <andrewdonshik@gmail.com>
2017-02-21 21:27:22 -0500

    py3
    Updated to python3, implemented load

--- scripts/autoconfig.py
+++ scripts/autoconfig.py
@@ -2,21 +2,22 @@ import sys, json, os
 
 class Configuration:
     def __init__(self):
-    	print "init"
-    	self.repositories = []
+        print("init")
+        self.repositories = []
         self.platform = ''
         self.hostfile = ''
     def printdict(self):
-    	print self.__dict__
+        print(self.__dict__)
     def autoconfig(self):
-        print sys.platform
+        print(sys.platform)
         self.platform = sys.platform
         if self.platform == 'darwin' or 'linux2':
-    	    self.hostfile = '/etc/hosts'
+            self.hostfile = '/etc/hosts'
         elif self.platform == 'win32':
-    	    self.hostfile = '%SystemRoot%\System32\drivers\etc\hosts'
+            self.hostfile = '%SystemRoot%\System32\drivers\etc\hosts'
         else:
             self.hostfile = str(input("Enter Plaintext Hostfile Location: "))
+        self.repositories.append("ppds.rollcagetech.com")
     def save(self):
         if os.path.isfile('config.json'):
             return('exists')
@@ -25,8 +26,10 @@ class Configuration:
             json.dump(self.__dict__, f)
             f.close()
             return('created')
-            
-a = Configuration()
-a.autoconfig()
-a.printdict()
-a.save()
+    def load(self):
+        if os.path.isfile('config.json'):
+            f = open('config.json', 'r+')
+            self.__dict__ = json.load(f)
+            f.close()
+        else:
+            return ("No File")
