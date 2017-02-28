5cea31df07db672891f83ffb36d7de6e4821b671
Andrew Donshik <andrewdonshik@gmail.com>
2017-02-28 17:19:39 -0500

    testing bugfix

--- exec_ppds.py
+++ exec_ppds.py
@@ -10,14 +10,14 @@ testconfig.load()
 testconfig.addrepo('ppds.rollcagetech.com')
 testconfig.forceaddrepo('ppds.rollcagetech.com')
 testconfig.definerepopriority()
-if testconfig.testrepo('rollcagetech.com') == 'down':
-    print ('down')
 testconfig.initrepolist()
 testconfig.repoobjectdict['repo.ppds.me'].loadpackagelist()
+testconfig.repoobjectdict['ppds.rollcagetech.com'].loadpackagelist()
+print(testconfig.repoobjectdict['ppds.rollcagetech.com'].packages)
 testconfig.repoobjectdict['repo.ppds.me'].enablepackage('main.json')
 testconfig.repoobjectdict['repo.ppds.me'].savepackagelist()
 testconfig.repoobjectdict['repo.ppds.me'].loadjson()
-
+testconfig.repoobjectdict['ppds.rollcagetech.com'].loadjson()
 patcher = classes.hostfilepatch.HostPatch(testconfig)
 
 patcher.createpatch()
