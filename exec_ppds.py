5a178b638d1ccdda0ceb0e5f0e6c798cc53eb40b
Andrew Donshik <andrewdonshik@gmail.com>
2017-03-16 07:59:52 -0400

    Version Flag

--- exec_ppds.py
+++ exec_ppds.py
@@ -5,7 +5,6 @@ import classes.repository
 import os
 import sys
 UID = os.getuid()
-print(os.getuid())
 isRoot = False
 if os.getuid() == 0:
     isRoot = True
@@ -24,10 +23,22 @@ def escalate(config):
         return 'root'
     if config.platform == 'linux' or 'darwin':
         if '.py' in sys.argv[0]:
-            args = ['python3.6'] + sys.argv
+            args = ['python3'] + sys.argv
         args = ['sudo'] + args
         os.execvp('sudo', args)
+    else:
+        return 'unsupported'
 
+if '--version' in sys.argv:
+    print("""
+    PPDS pre-alpha 0.0.1
+
+    Private Peer Domain Syndication: copyright 2017 by Andrew Donshik
+    Licensed under the GPL v3.0
+    THIS SOFTWARE IS PROVIDED AS-IS
+    WITHOUT WARRANTY OR LIABILITY AS THE LAW PERMITS
+    """)
+    exit()
 
 print(sys.argv)
 testconfig = classes.config.Configuration()
