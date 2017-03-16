fd4db881dec29147b4ac982198d24f57df125370
Andrew Donshik <andrewdonshik@gmail.com>
2017-03-16 07:37:02 -0400

    invoke sudo

--- exec_ppds.py
+++ exec_ppds.py
@@ -2,9 +2,36 @@
 import classes.config
 import classes.hostfilepatch
 import classes.repository
+import os
+import sys
+UID = os.getuid()
+print(os.getuid())
+isRoot = False
+if os.getuid() == 0:
+    isRoot = True
 
+
+def CheckAccess(location):
+    if os.access(location, os.w_OK):
+        return True
+    else:
+        return 'rootneeded'
+
+
+def escalate(config):
+    # spawn sudo command and rerun
+    if isRoot:
+        return 'root'
+    if config.platform == 'linux' or 'darwin':
+        if '.py' in sys.argv[0]:
+            args = ['python3.6'] + sys.argv
+        args = ['sudo'] + args
+        os.execvp('sudo', args)
+
+
+print(sys.argv)
 testconfig = classes.config.Configuration()
-
+escalate(testconfig)
 testconfig.autoconfig()
 testconfig.makerepofolders()
 testconfig.save()
